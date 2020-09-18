f0be7de97a34617f3bf022a166e495255fa1e711
Ale <Rale@JAVIERs-MacBook-Air.local>
2020-09-17 21:45:11 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 print ('[------------------------------]')
 print ('[      Select the Imposter     ]')
-print ('[  Select: a color Red    (1)  ]')
-print ('[  Select: a color Blue   (2)  ]')
-print ('[  Select: a color Pink   (3)  ]')
-print ('[  Select: a color Green  (4)  ]')
-print ('[  Select: a color Orange (5)  ]')
-print ('[  Select: a color Yellow (6)  ]')
-print ('[  Select: a color Cyan   (7)  ]')
-print ('[  Select: a color Black  (8)  ]')
-print ('[  Select: a color White  (9)  ]')
+print ('[  Select: a color Red         ]')
+print ('[  Select: a color Blue        ]')
+print ('[  Select: a color Pink        ]')
+print ('[  Select: a color Green       ]')
+print ('[  Select: a color Orange      ]')
+print ('[  Select: a color Yellow      ]')
+print ('[  Select: a color Cyan        ]')
+print ('[  Select: a color Black       ]')
+print ('[  Select: a color White       ]')
 print ('[------------------------------]')
 print("You have 5 tries to guess the impostor")
 
 numberOfTries = 5
 triesLeft = numberOfTries
-rightAnswer = 8
+rightAnswer = "pink"
 
 for x in range(numberOfTries):
     triesLeft -= 1
-    number = int(input('Enter your answer: '))
+    color = input('Enter your answer: ')
 
-    if (number == rightAnswer):
+    if (color.lower() == rightAnswer.lower()):
         print ("Nice! You found the Imposter!")
     else:
         print ("Wrong answer, try again")
